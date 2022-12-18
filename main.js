1f391160275487b4194f0c554dad0417c9711db6
Diego Pasaye <pasayealvarado@gmail.com>
2022-12-17 23:47:40 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const email = document.querySelector(".email")
+const incorrecto = document.querySelector(".incorrecto")
+const boton = document.querySelector(".boton")
+
+
+let emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+let correcto = ""
+const validacion = () =>{
+    console.log(email.value)
+    if (emailRegex.test(email.value)) {
+        console.log("bien")
+}else{
+    incorrecto.style.display = "block"
+    email.style.border ="0.1vw solid hsl(354, 100%, 66%)"
+}}
